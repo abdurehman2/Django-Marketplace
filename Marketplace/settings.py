e989467a65ac03f50e8b46419ea269d399866aa7
Abdur Rehman <rehmanabdur824@gmail.com>
2023-07-30 16:55:21 +0500

    Final Commit

--- Marketplace/settings.py
+++ Marketplace/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conversation',
     'core',
     'item',
-    'dashboard',
+    'dashboard', 
 ]
 
 MIDDLEWARE = [
--- item/views.py
+++ item/views.py
@@ -12,6 +12,7 @@ def items(request):
 
     if category_id:
         items = items.filter(category_id=category_id)
+        
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
